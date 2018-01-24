37568c0b79c63dbc57fc520359c3d709d00520ab
sapphire <info@sapphire.sh>
2018-01-25 03:27:34 +0900

    commit

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -18,61 +18,75 @@ public class CameraController : MonoBehaviour {
 	public float velocity = 10.0f;
 
 	private void UpdatePosition(float x) {
-		x *= 0.1f;
+		x *= 0.2f * transform.parent.localScale.x;
 
 		float _forward = 0.0f;
 		float _leftward = 0.0f;
 		float _upward = 0.0f;
 
+		bool isUpdated = false;
 		if(Input.GetKey(forward)) {
 			_forward += x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(backward)) {
 			_forward -= x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(leftward)) {
 			_leftward += x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(rightward)) {
 			_leftward -= x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(upward)) {
 			_upward += x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(downward)) {
 			_upward -= x;
+			isUpdated = true;
 		}
 
-		var p = new Vector3(-_leftward, _upward, _forward);
-
-		transform.position += transform.rotation * p;
+		if(isUpdated) {
+			var p = new Vector3(-_leftward, _upward, _forward);
+			transform.position += transform.rotation * p;
+		}
 	}
 
 	private void UpdateRotation(float x) {
 		x *= 2.0f;
 
-		var _e = transform.rotation.eulerAngles;
+		var _e = transform.localRotation.eulerAngles;
 		float _pitch = _e.x;
 		float _yaw = _e.y;
 		float _roll = _e.z;
 
+		bool isUpdated = false;
 		if(Input.GetKey(pitchUpward)) {
 			_pitch += x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(pitchDownward)) {
 			_pitch -= x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(yawLeftward)) {
 			_yaw += x;
+			isUpdated = true;
 		}
 		if(Input.GetKey(yawRightward)) {
 			_yaw -= x;
+			isUpdated = true;
 		}
 
-		var e = new Vector3(_pitch, _yaw, _roll);
-		var q = Quaternion.Euler(e);
-
-		transform.localRotation = q;
+		if(isUpdated) {
+			var e = new Vector3(_pitch, _yaw, _roll);
+			var q = Quaternion.Euler(e);
+			transform.localRotation = q;
+		}
 	}
 
 	void Update() {
--- Assets/ProjectionMatrix.cs
+++ Assets/ProjectionMatrix.cs
@@ -15,38 +15,30 @@ public class ProjectionMatrix : MonoBehaviour {
 	[SerializeField]
 	Material material;
 
+	[SerializeField]
+	GameObject focus;
+
 	Shader shader;
 
-	new private Camera camera;
-
-	void Awake() {
-		camera = GetComponent<Camera>();
-	}
-
-	void Update() {
+	void Start() {
 		switch(type) {
 		case CameraType.Left:
-			camera.projectionMatrix = camera.projectionMatrix;
 			break;
 		case CameraType.Right:
-			Debug.Log(fov);
-			var matrix = Matrix4x4.Perspective(fov, 16.0f / 9.0f, 0.3f, 1000.0f);
-			camera.projectionMatrix = Matrix4x4.Scale(Vector3.one * 0.5f) * matrix;
+			var parent = transform.parent;
+			var focusPos = focus.transform.position;
+			var invPos = parent.InverseTransformPoint(focusPos);
+			var invRot = Quaternion.Inverse(parent.rotation);
+
+			parent.localScale = Vector3.one * 2.0f;
+			parent.localPosition = -(invRot * invPos * 2.0f) + focusPos;
+			parent.localRotation = invRot;
+
 			break;
 		}
 	}
 	
     void OnRenderImage(RenderTexture source, RenderTexture destination) {
 		Graphics.Blit(source, destination, material);
-	}
-
-	float fov = 60.0f;
-
-	const float padding = 50.0f;
-
-	void OnGUI() {
-		if(type == CameraType.Right) {
-			fov = GUI.VerticalSlider(new Rect(Screen.width - padding, padding, 10.0f, Screen.height - padding * 2.0f), fov, 1.0f, 179.0f);
-		}
 	}
 }
